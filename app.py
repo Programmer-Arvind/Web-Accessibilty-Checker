c2dc1668a0e8b17c769c0c1902c563cf47ae159b
dotdev262 <dotdev262@gmail.com>
2024-06-30 22:51:07 +0530

    Final Submission

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from scripts.webchecker import run_lighthouse_command
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET","POST"])
 def index():
+    if request.method == "POST":
+        url = request.form.get("user-input-url")
+        if request.form.get("view") == "desktop-view":
+            run_lighthouse_command(url, view="desktop")
+        run_lighthouse_command(url)
+        return render_template('report.html')
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
--- scripts/webchecker.py
+++ scripts/webchecker.py
@@ -1,26 +1,25 @@
 import subprocess
 import json
 
-# Replace this with the full path to your Lighthouse installation if necessary
-lighthouse_path = 'lighthousepath'
 
 # URL of the website you want to check
-url = 'url to check'
+url = 'https://www.youtube.com/'
 
 # Command to run Lighthouse and output the results in JSON format
 command = [
-    lighthouse_path,
-    url,
-    '--output=json',
+    "npx",
+    "lighthouse",
     '--output=html',
-    '--output-path=report.json',
+    '--output-path=./templates/report.html',
     # # '--quiet',
-    # "--preset=desktop",
-    "--view"
+    # "--preset=desktop"
 ]
 
 # Function to run the command and handle errors
-def run_lighthouse_command(command):
+def run_lighthouse_command(url, view=None):
+    command.append(url)
+    if view:
+        command.append("--preset=desktop")
     try:
         result = subprocess.run(command, capture_output=True, text=True)
         if result.returncode != 0:
@@ -35,19 +34,16 @@ def run_lighthouse_command(command):
         return None
 
 # Run the command
-result = run_lighthouse_command(command)
+# result = run_lighthouse_command(command)
 
-if result:
-    # Load the Lighthouse report from the generated file with explicit UTF-8 encoding
-    try:
-        with open('report.json', 'r', encoding='utf-8') as f:
-            lighthouse_report = json.load(f)
+# if result:
+#     # Load the Lighthouse report from the generated file with explicit UTF-8 encoding
+#     try:
+#         with open('report.json', 'r', encoding='utf-8') as f:
+#             lighthouse_report = json.load(f)
 
-        # Print the results
-        print(json.dumps(lighthouse_report, indent=2))
+#         # Print the results
+#         print(json.dumps(lighthouse_report, indent=2))
 
-        # Save the results to a file
-        with open('lighthouse_report.json', 'w', encoding='utf-8') as f:
-            json.dump(lighthouse_report, f, indent=2)
-    except Exception as e:
-        print(f"Error reading the Lighthouse report: {e}")
+#     except Exception as e:
+#         print(f"Error reading the Lighthouse report: {e}")
